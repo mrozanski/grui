5fba58d1299316a8ed8b5794dee48a58423d9317
mrozanski <mariano@dotsur.co>
2025-08-19 15:47:18 -0600

    Search page, favicon, header illustrations

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,10 +4,10 @@ import { Navbar } from "./navbar"
 
 export function Header() {
   return (
-    <header className="border-b border-border bg-surface px-6 pt-4 pb-2 relative">
+    <header className="border-b border-border bg-surface px-6 pt-4 pb-2 relative header-bg-image">
       <div className="flex flex-col items-center justify-center max-w-7xl mx-auto w-full">
         <div>
-          <h1 className="text-3xl font-brand text-[var(--brand-color)] text-center">
+          <h1 className="text-3xl font-brand text-[var(--brand-color)] text-center header-title">
             <span className="text-lg">ELECTRIC</span><br ></br>GUITAR REGISTRY
           </h1>
           <p className="text-sm text-muted-foreground text-center">
